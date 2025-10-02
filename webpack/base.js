96f225a8c7d8a89332344b830afe20a18f67dd0a
Vladimir Popov <aphropop@mail.ru>
2025-10-02 14:28:19 +0300

    first commit

---
+++ webpack/base.js
@@ -0,0 +1,40 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/scripts/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(png|mp3|jpe?g)$/i,
+        use: "file-loader"
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "bundle.min.js",
+    clean: true  // очищать dist перед сборкой, если используешь Webpack 5
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true)
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ]
+};
